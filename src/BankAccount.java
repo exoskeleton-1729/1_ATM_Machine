083b66cd8e57d99e7b4ea9cc089467f994db3f2b
Anika Iyer <anika.sule.iyer@gmail.com>
2022-08-30 14:21:14 -0700

    Created Method Stubs
    Kind of exactly like it sounds like. Also made a HashMap. Surely I will use this. But yes made stubs for all the methods.

---
+++ src/BankAccount.java
@@ -0,0 +1,35 @@
+import java.util.*;
+public class BankAccount {
+	
+	private HashMap<Long, Double> accounts = new HashMap<Long, Double> ();
+	
+	public BankAccount()
+	{
+		
+	}
+	
+	public void openAccount(Long accountNumber)
+	{
+		
+	}
+	
+	public void openAccount(Long accountNumber, double initialDeposit)
+	{
+		
+	}
+	
+	public void closeAccount(Long accountNumber)
+	{
+		
+	}
+	
+	public boolean depositMoney(Long accountNumber, double deposit)
+	{
+		return false;
+	}
+	
+	public boolean withdrawMoney(Long accountNumber, double withdraw)
+	{
+		return false;
+	}
+}
